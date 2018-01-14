5ea4f1ded74b5fd7ec6a35d82a084d7c0b3a2c6d
Colleen Saltarelli <colleen@digitallyft.com>
2018-01-14 13:55:15 -0500

    tidy up time router

--- time/router.js
+++ time/router.js
@@ -1,4 +1,3 @@
-
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,8 +8,8 @@ moment().format();
 
 mongoose.Promise = global.Promise;
 
-const {User} = require('../users/models');
-const {Time} = require('./models');
+const { User } = require('../users/models');
+const { Time } = require('./models');
 const router = express.Router();
 const jsonParser = bodyParser.json();
 const jwtAuth = passport.authenticate('jwt', { session: false });
@@ -18,74 +17,74 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 //add a new clock-in entry
 router.post('/clockin', jsonParser, jwtAuth, (req, res) => {
   console.log('hitting the clock in route');
-    Time
-    .create({userRef: req.user._id, startTime: moment()})
+  Time
+    .create({ userRef: req.user._id, startTime: moment() })
     .then(time => res.status(201).json(time))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 //add a new clock-out entry
 router.post('/clockout', jsonParser, jwtAuth, (req, res) => {
-    Time.findById(req.body.id)
-    .update({endTime: moment()})
+  Time.findById(req.body.id)
+    .update({ endTime: moment() })
     .then(time => res.status(201).json(time))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 router.get('/entries/:id', jwtAuth, jsonParser, (req, res) => {
-    if (!req.user.admin) {
-      return res.send('you are not admin').status(401);
-    }
-    else {
-    Time.find({userRef: req.params.id})
+  if (!req.user.admin) {
+    return res.send('you are not admin').status(401);
+  }
+  else {
+    Time.find({ userRef: req.params.id })
       .then(entries => res.json(entries.map(entry => entry.apiRepr())))
-      .catch(err => res.status(500).json({message: 'Internal server error'}));
-    }
-  });
-  
-  router.get('/entries', jwtAuth, jsonParser, (req, res) => {
-    Time.find({userRef: req.user._id})
+      .catch(err => res.status(500).json({ message: 'Internal server error' }));
+  }
+});
+
+router.get('/entries', jwtAuth, jsonParser, (req, res) => {
+  Time.find({ userRef: req.user._id })
     .then(entries => res.json(entries.map(entry => entry.apiRepr())))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-  });
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
 
-  router.get('/:id', jwtAuth, (req, res) => {
-    if (!req.user.admin) {
-      return res.send('you are not admin').status(401);
-    }
-    else {
+router.get('/:id', jwtAuth, (req, res) => {
+  if (!req.user.admin) {
+    return res.send('you are not admin').status(401);
+  }
+  else {
     Time.find(req.params._id)
-      .then(entries => res.json({message: 'admin'}))
-      .catch(err => res.status(500).json({message: 'Internal server error'}));
-    }
-  });
-  
-  router.get('/', jwtAuth, (req, res) => {
-    Time.find(req.user._id)
-    .then(entries => res.json({message: 'user'}))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-  });
+      .then(entries => res.json({ message: 'admin' }))
+      .catch(err => res.status(500).json({ message: 'Internal server error' }));
+  }
+});
 
-  // admin to update another users timesheet
+router.get('/', jwtAuth, (req, res) => {
+  Time.find(req.user._id)
+    .then(entries => res.json({ message: 'user' }))
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+// admin to update another users timesheet
 router.put('/:id', jwtAuth, jsonParser, (req, res) => {
   if (!req.user.admin) {
     return res.send('you are not admin').status(401);
   }
   Time
-  .findByIdAndUpdate(req.params.id, req.body)
-  .then(time => res.json(time))
-  .catch(err => res.status(500).json({message: 'Something went wrong'}));
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then(time => res.json(time))
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
 });
 
-  // admin to delete another users timesheet
-  router.delete('/:id', jwtAuth, (req, res) => {
-    if (!req.user.admin) {
-      return res.send('you are not admin').status(401);
-    }
-    Time
+// admin to delete another users timesheet
+router.delete('/:id', jwtAuth, (req, res) => {
+  if (!req.user.admin) {
+    return res.send('you are not admin').status(401);
+  }
+  Time
     .findByIdAndRemove(req.params.id)
     .then(time => res.json(time))
-    .catch(err => res.status(500).json({message: 'Something went wrong'}));
-  });
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+});
 
-module.exports = {router};
+module.exports = { router };
